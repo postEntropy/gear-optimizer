3a809cfabb4622c74c6fb34be957c001f016552a
postEntropy <garciagerytch@icloud.com>
2026-04-06 10:00:42 -0300

    style(ui): set NGU input boxes to width 40px

--- src/components/Content/NGUs.js
+++ src/components/Content/NGUs.js
@@ -409,17 +409,17 @@ class NGUComponent extends Component {
                                                     <TableCell>
                                                         <TextField type="number" value={stats[pos].normal}
                                                             onFocus={this.handleFocus} onChange={(e) => this.handleChange(e, 'normal', pos, isMagic)}
-                                                            inputProps={{ step: "any" }} sx={{ width: 50 }} hiddenLabel size="small" />
+                                                            inputProps={{ step: "any" }} sx={{ width: 40 }} hiddenLabel size="small" />
                                                     </TableCell>
                                                     <TableCell>
                                                         <TextField type="number" value={stats[pos].evil}
                                                             onFocus={this.handleFocus} onChange={(e) => this.handleChange(e, 'evil', pos, isMagic)}
-                                                            inputProps={{ step: "any" }} sx={{ width: 50 }} hiddenLabel size="small" />
+                                                            inputProps={{ step: "any" }} sx={{ width: 40 }} hiddenLabel size="small" />
                                                     </TableCell>
                                                     <TableCell>
                                                         <TextField type="number" value={stats[pos].sadistic}
                                                             onFocus={this.handleFocus} onChange={(e) => this.handleChange(e, 'sadistic', pos, isMagic)}
-                                                            inputProps={{ step: "any" }} sx={{ width: 50 }} hiddenLabel size="small" />
+                                                            inputProps={{ step: "any" }} sx={{ width: 40 }} hiddenLabel size="small" />
                                                     </TableCell>
                                                     <TableCell sx={isBestAny ? { fontWeight: 'bold' } : {}}>{'×' + shorten(bonus * 100) + '%'}</TableCell>
                                                     <TableCell sx={isBestNormal ? { fontWeight: 'bold' } : {}}>
